6f4b55325b1254dc91743fdedbcb4d50df538d85
Sarah Jimenez <57870970+sarahmarie23@users.noreply.github.com>
2021-08-29 15:06:55 -0700

    Create Algorithms.java

---
+++ Algorithms.java
@@ -0,0 +1,21 @@
+public class Algorithms {
+    public static void main(String[] args) {
+        
+        // Insertion-Sort
+        int[] insArr = {5,2,4,6,1,3};
+		System.out.println("Insertion Sort: " + Arrays.toString(insertionSort(insArr)));
+        
+    }
+    public static int[] insertionSort(int[] arr) {
+		for(int j = 1; j < arr.length; j++) {
+			int key = arr[j];
+			int i = j - 1;
+			while (i >= 0 && arr[i] > key) {
+				arr[i + 1] = arr[i];
+				i = i - 1;
+			}
+			arr[i + 1] = key;
+		}
+		return arr;
+	}
+}
