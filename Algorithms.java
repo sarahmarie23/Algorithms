c93dda5c6e12c1eaa0ceef5154ea9c4983c6d5fc
Sarah Jimenez <57870970+sarahmarie23@users.noreply.github.com>
2021-08-29 15:24:20 -0700

    Update Algorithms.java

--- Algorithms.java
+++ Algorithms.java
@@ -4,6 +4,7 @@ public class Algorithms {
         // Insertion-Sort
         int[] insArr = {5,2,4,6,1,3};
 		System.out.println("Insertion Sort: " + Arrays.toString(insertionSort(insArr)));
+		System.out.println("Insertion Sort in nonincreasing order: " + Arrays.toString(nonincreasingInsertionSort(insArr)));
         
     }
     public static int[] insertionSort(int[] arr) {
@@ -11,6 +12,19 @@ public class Algorithms {
 			int key = arr[j];
 			int i = j - 1;
 			while (i >= 0 && arr[i] > key) {
+				arr[i + 1] = arr[i];
+				i = i - 1;
+			}
+			arr[i + 1] = key;
+		}
+		return arr;
+	}
+	
+	public static int[] nonincreasingInsertionSort(int[] arr) {
+		for(int j = 1; j < arr.length; j++) {
+			int key = arr[j];
+			int i = j - 1;
+			while (i >= 0 && arr[i] < key) {
 				arr[i + 1] = arr[i];
 				i = i - 1;
 			}
