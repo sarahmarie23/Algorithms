38f722808651c59a8c6fcd42d7c95d72a836ce92
Sarah Jimenez <57870970+sarahmarie23@users.noreply.github.com>
2021-11-09 11:22:40 -0800

    Create mergeSort.h

---
+++ mergeSort/mergeSort.h
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+
+using namespace std;
+
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+void mergeSort(int *arr, int min, int max);
+
+#endif /* MERGESORT_H */
